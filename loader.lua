cf517310c330a0ccfac24fd409a5bfde7bd7c1e5
JoshaDeveloper <154029517+JoshaDeveloper@users.noreply.github.com>
2023-12-17 09:51:40 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -109,7 +109,7 @@ ballFolder.ChildAdded:Connect(function(v)
                         if curDistance < 17 then
                             parryButton:Fire()
                         end
-                        if curDistance / maxSpeed < 0.35 then
+                        if curDistance / maxSpeed < 0.5 then
                             closeRangecount = closeRangecount + 1
                             if closeRangecount >= 3 then
                                 spamCase = true
