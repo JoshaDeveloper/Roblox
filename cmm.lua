57612964799d44a48d15ba8571d93e90a3a35487
JoshaDeveloper <154029517+JoshaDeveloper@users.noreply.github.com>
2023-12-20 20:12:36 +0700

    Update cmm.lua

--- cmm.lua
+++ cmm.lua
@@ -146,7 +146,7 @@ getgenv().jaidenConnection.heartBeat = game:GetService('RunService').Heartbeat:C
         visualPart.Size = Vector3.new(maxVelocity / 6, maxVelocity / 6, maxVelocity / 6)
         visualPart.Position = character.HumanoidRootPart.Position
         if character:FindFirstChild("Highlight") then
-            if (character.HumanoidRootPart.Position - ball.Position).Magnitude <= maxVelocity * 1.25 then
+            if (character.HumanoidRootPart.Position - ball.Position).Magnitude <= maxVelocity * 1.75 then
                 if isParried(ball) < 0.25 then
                     parryButton:Fire()
                 end
